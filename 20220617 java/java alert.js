9d113e26837fb40d067374edcf1e039cc9f02abf
yong <kkby826@gmail.com>
2022-06-17 14:56:49 +0900

    second

---
+++ 20220617 java/java alert.js
@@ -0,0 +1,77 @@
+/**
+ * 대화 상자 (alert, confirm, prompt)
+ */
+
+// alert('안녕하세요.');
+
+// var result=confirm('안녕하세요')
+
+// alert(result) // 확인:ture, 취소:false
+
+// result 값이 확인 (true)이면
+// 삭제
+// result 값이 취소 (false)이면
+// 원래대로.
+
+//var result=prompt("값을 입력하세요.")
+//alert(result)
+
+/**
+* 문제 1번
+let carName = 'Volvo';
+
+* 문제 2번
+2. let x = 50
+
+* 문제 3번
+3. let x = 5 /   let y = 10 /   x+y = 15
+
+* 문제 4번
+4. alert (z=x+y)
+
+////////////////////////////////////////////////////
+
+/** 문제 5번
+
+let con_test = confirm("진짜로 삭제하시겠습니까?");
+
+*if(con_test == true){
+  alert("삭제됨");
+}
+*else if(con_test == false){
+  alert("취소됨");
+}
+
+*/
+////////////////////////////////////////////////////
+
+/** 문제 6번
+let str1 = prompt("몇", "");
+let str2 = prompt("더하기 몇은?", "");
+
+let num1;
+let num2;
+
+num1 = Number(str1);
+num2 = Number(str2);
+
+alert(num1 + num2);
+
+*/
+///////////////////////////////////////////////////
+
+/** 문제 7번
+
+let agePrompt = prompt('나이를 입력하세요.');
+
+let age = Number(agePrompt);
+
+if(age >= 19) {
+  alert('인증됨');
+}
+else if (age <= 19) {
+    alert('인증되지않음');
+} 
+
+*/
+
---
+++ 20220617 java/연습.js
@@ -0,0 +1,10 @@
+let str1 = prompt("몇", "");
+let str2 = prompt("더하기 몇은?", "");
+
+let num1;
+let num2;
+
+num1 = Number(str1);
+num2 = Number(str2);
+
+alert(num1 + num2);
