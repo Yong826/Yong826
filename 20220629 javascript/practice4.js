db43d723660d82aeadf659db253cae18f4625c66
Yong826 <105827577+Yong826@users.noreply.github.com>
2022-06-29 23:27:11 +0900

    20220630

--- 20220629 javascript/practice4.js
+++ 20220629 javascript/practice4.js
@@ -7,18 +7,9 @@ $(document).ready(function(){
 
 
 
-    $("select").blur(function(){
-        let sum = parseInt($(".1").val()) * parseInt($("#val1").val())
-        let sum1 = parseInt($(".2").val()) * parseInt($("#val1").val())
-        let sum2 = parseInt($(".3").val()) * parseInt($("#val1").val())
-        let sum3 = parseInt($(".4").val()) * parseInt($("#val1").val())
-        let sum4 = parseInt($(".5").val()) * parseInt($("#val1").val())
-
-        $("#val2").val(sum);
-        $("#val2").val(sum1);
-        $("#val2").val(sum2);
-        $("#val2").val(sum3);
-        $("#val2").val(sum4);
+    $("select").blur(function(){  
+    let sum  = $("#val3 option:selected").val() * $("#val1").val();    
+    $("#val2").html(sum)    
     })
 
 })
