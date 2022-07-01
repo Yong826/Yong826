4f9f4f990acf2604bf4f8671e62e261319061e53
Yong <kkby826@gmail.com>
2022-07-01 16:44:41 +0900

    20220701

---
+++ Java Practice/20220701/src/Variable.java
@@ -0,0 +1,26 @@
+public class Variable{
+    // 자바에서의 변수를 알아보자 
+    public static void main(String [] args) {
+        // true 값을 b 변수에 초기화 해보자
+        boolean b = true;
+
+        // 'a' 라는 문자를 c변수에 초기화 해보자
+        char c = 'a';
+
+        // "안녕하세요" 라는 문자를 d 변수에 초기화 해보자
+        String d = "안녕하세요";
+
+        // 10을 a 변수에 초기화 해보자
+        // int a = 10;
+
+        // byte a = (byte)130;
+
+        // System.out.println(a);
+
+        // long e = 100000000000L; 
+
+        float f = 3.14f;
+
+
+    }
+}
